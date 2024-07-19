cd85e3d004abc701834fc695b98ab6f8c68ef52c
Ay-slim <ayooluwaadedipe@gmail.com>
2024-07-19 23:06:21 +0000

    Arrays

---
+++ arrays_slices/arrays_slices.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// arrays are fixed length data structures can be initialized with the format [length]type
+	a := [5]int32{} // If initialized without values, defaults to zero
+	fmt.Println(a)
+
+	b := [5]int32{1, 2, 3, 4, 5} // You can specify on declaration liek so
+	fmt.Println(b)
+
+	c := [...]int32{1, 2, 3, 4} // Rather than specify size, you can use the ... operator to let the compiler infer size
+	fmt.Println(c)
+
+	d := [...]int32{1, 2: 800} // You can use the index to specify the value at that indexrather than typing it in directly, printed value: [1 0 800]
+	fmt.Println(d)
+
+	e := [3][3]int{} // Flex on the syntax
+	for i:=0; i < 3; i++ {
+		for j:=0; j < 3; j++ {
+			e[i][j] = i + j
+		}
+	}
+	fmt.Println(e)
+}
